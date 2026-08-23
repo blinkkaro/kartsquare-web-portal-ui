7ef5c7a2241a9f5f2bba2510121a5536fa9499e1
Yash soni <yashsoni@heyneo.so>
2026-08-23 13:10:40 +0530

    Revert "Go Live option added"
    This reverts commit d9aa13341ea680523766e208403dd31500502443.

--- features/i18n/TranslationContext.tsx
+++ features/i18n/TranslationContext.tsx
@@ -1277,34 +1277,7 @@ export type TranslationKey =
   | "Enter_value"
   | "Select_option"
   | "please_provide_rating"
-  | "rate_your_experience"
-  // Live location
-  | "liveLocationTitle"
-  | "liveLocationLive"
-  | "liveLocationOnBody"
-  | "liveLocationOffBody"
-  | "liveLocationToggleAria"
-  | "liveLocationThrottled"
-  | "liveLocationRadius"
-  | "liveLocationRadiusDefault"
-  | "liveLocationLastUpdate"
-  | "liveLocationAccuracy"
-  | "liveLocationQueued"
-  | "liveLocationChipTooltip"
-  | "liveLocationExpiresIn"
-  | "liveLocationConsentTitle"
-  | "liveLocationConsentIntro"
-  | "liveLocationConsentWhatTitle"
-  | "liveLocationConsentWhatBody"
-  | "liveLocationConsentWhoTitle"
-  | "liveLocationConsentWhoBody"
-  | "liveLocationConsentExpiryTitle"
-  | "liveLocationConsentExpiryBody"
-  | "liveLocationConsentStopTitle"
-  | "liveLocationConsentStopBody"
-  | "liveLocationConsentBrowserNote"
-  | "liveLocationConsentAccept"
-  | "notNow";
+  | "rate_your_experience";
 
 // Sample dictionaries
 const dictionaries: Record<"en", Record<TranslationKey, string>> = {
